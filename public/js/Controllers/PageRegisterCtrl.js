1a4041815bcf35cb06c1b5ae1329d8e256c1dc9e
Syberiaaa <gal35118958@yandex.ru>
2015-10-08 18:40:50 +0800

    registr issue#1

--- public/js/Controllers/PageRegisterCtrl.js
+++ public/js/Controllers/PageRegisterCtrl.js
@@ -1,4 +1,17 @@
-PagesControllers.controller('RegisterCtrl', ['$scope',
-    function($scope) {
+PagesControllers.controller('RegisterCtrl', ['$scope', '$http', '$location',
+    function($scope, $http, $location) {
+        $scope.name = '';
+        $scope.email = '';
+        $scope.password = '';
+
+        $scope.user = function() {
+            $http.put('/api/users', {
+                name: $scope.name,
+                email: $scope.email,
+                password: $scope.password
+            }).then(function(response) {
+                $location.path('/login');
+            });
+        }
     }
 ]);
