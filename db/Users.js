43b5720ee9ff5a152e440b22e1d8d855b4a7d5d1
Nikolay <lonhcakov2020@mail.ru>
2015-10-22 16:27:19 +0900

    kolyan2

--- db/Users.js
+++ db/Users.js
@@ -1,32 +1,3 @@
-
-var passwordHash = require('password-hash');
-
-module.exports = function(mongoose) {
-  var usersSchema = mongoose.Schema({
-    name: String,
-    email: String,
-    role: String,
-    password: String
-  });
-
-  usersSchema.pre('save', function (next) {
-    if (!passwordHash.isHashed(this.password)) {
-      this.password = passwordHash.generate(this.password);
-    }
-    next();
-  });
-
-  var Users = mongoose.model('Users', usersSchema);
-
-  //Users.count({}, function(err, cnt) {
-  //  if (cnt === 5) {
-      var admin = new Users({name: 'Petr', email: 'pety@gmail.com', role: 'user', password: '123456'});
-      admin.save(function(err) {
-        if (err) console.error(err);
-      });
-  //  }
-  //});
-
 var passwordHash = require('password-hash');
 var Utils = require("../Utils/Utils.js");
 
@@ -65,4 +36,4 @@ module.exports = function(mongoose) {
     }
   });
 
-}};
+};
