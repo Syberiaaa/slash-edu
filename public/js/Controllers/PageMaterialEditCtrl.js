b0da0d3c59a8030f4f04b9b64ce75b81f20828d7
mariyaderevyagina2015 <mariyaderevyagina1992@gmail.com>
2015-10-22 16:40:10 +0800

    commit for MathJax issue

--- public/js/Controllers/PageMaterialEditCtrl.js
+++ public/js/Controllers/PageMaterialEditCtrl.js
@@ -1,27 +1,32 @@
-PagesControllers.controller('MaterialEditCtrl', ['$scope', '$http', '$location', '$routeParams',
-  function($scope, $http, $location, $routeParams) {
-    $scope.name = '';
-    $scope.type = 'text';
-    $scope.src = '';
-    $scope.html = '';
+PagesControllers.controller('MaterialEditCtrl', ['$scope', '$http', '$location', '$routeParams', '$sce', '$parse',
+    function($scope, $http, $location, $routeParams, $sce, $parse) {
+        $scope.name = '';
+        $scope.type = 'text';
+        $scope.src = '';
+        $scope.html = '';
 
-    $scope.materialId = $routeParams.materialId;
+        $scope.materialId = $routeParams.materialId;
 
-    $scope.preview = function() {
-      $http.post('/api/materials/preview', {src: $scope.src})
-        .then(function(response) {
-          $scope.html = response.data;
-        });
-    };
+       $scope.preview = function() {
+            $http.post('/api/materials/preview', {src: $scope.src})
+                .then(function(response) {
+                    $scope.html = $sce.trustAsHtml(response.data);
+                    MathJax.Hub.Reprocess(function() {
+                        console.log(arguments);
+                    });
 
-    $scope.add = function() {
-      $http.put('/api/materials', {
-        name: $scope.name,
-        type: $scope.type,
-        src: $scope.src
-      }).then(function(response) {
-        $location.path('/materials');
-      });
+
+                });
+        };
+
+        $scope.add = function() {
+            $http.put('/api/materials', {
+                name: $scope.name,
+                type: $scope.type,
+                src: $scope.src
+            }).then(function(response) {
+                $location.path('/materials');
+            });
+        }
     }
-  }
 ]);
