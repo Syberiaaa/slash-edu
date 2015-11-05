6c42afcd54e220ef0c22121bd0fe1a8c8ce82e3e
Sergey Reymerov <sergeyreym@gmail.com>
2015-11-05 13:08:30 +0800

    UI quality increased Mistyping bug in StudentCourse model is fixed

--- db/Courses.js
+++ db/Courses.js
@@ -2,7 +2,8 @@ module.exports = function(mongoose) {
     var coursesSchema = mongoose.Schema({
         name: String,
         author: mongoose.Schema.Types.ObjectId,
-        materials: [mongoose.Schema.Types.ObjectId]
+        materials: [mongoose.Schema.Types.ObjectId],
+        instructors: [mongoose.Schema.Types.ObjectId]
     });
 
     var Courses = mongoose.model('Courses', coursesSchema);
