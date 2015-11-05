606d982ff3164ee9f3e5b2cf2af8766a864d1377
Sergey Moiseenko <sergeymoiseenko.i@mail.ru>
2015-11-05 17:25:47 +0800

    Испарвлена ошибка связанная с именем папки utils

--- db/Users.js
+++ db/Users.js
@@ -1,5 +1,5 @@
 var passwordHash = require('password-hash');
-var Utils = require("../Utils/Utils.js");
+var Utils = require("../utils/Utils.js");
 
 module.exports = function(mongoose) {
   var  Schema = mongoose.Schema;
