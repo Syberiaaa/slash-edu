0096456780f7313311be017244d20c6cf195d8ac
Sergey Reymerov <sergeyreym@gmail.com>
2015-11-11 18:11:04 +0800

    Go link added

---
+++ public/js/Controllers/PageGoMaterialCtrl.js
@@ -0,0 +1,6 @@
+PagesControllers.controller('GoMaterialCtrl', ['$scope',
+    function($scope) {
+        $scope.courseName = 'Awesome Course';
+        $scope.materialName = 'Some Material';
+    }
+]);
