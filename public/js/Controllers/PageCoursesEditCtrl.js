6a22a24d093cbc397c82c66a9526727a5e4b331f
mizyukan <mizyukan@mail.ru>
2015-11-12 16:03:20 +0800

    #20 Создать страницу создания/редактирования курса

--- public/js/Controllers/PageCoursesEditCtrl.js
+++ public/js/Controllers/PageCoursesEditCtrl.js
@@ -1,46 +1,25 @@
 PagesControllers.controller('CoursesEditCtrl', ['$scope', '$http',
     function($scope, $http) {
+        $scope.courseMaterials = [];
+
         $http.get('/api/materials')
             .then(function(response){
                 $scope.materials = response.data;
             }
         );
+
+        $scope.addCourseMaterial = function(material){
+            $scope.courseMaterials.push(material);
+            var i = $scope.materials.indexOf(material);
+            $scope.materials.splice(i,1)
+    }
+
+        $scope.removeCourseMaterial = function(material) {
+            var i = $scope.courseMaterials.indexOf(material);
+            $scope.courseMaterials.splice(i,1)
+            $scope.materials.push(material)
+        }
+
     }
 ]);
 
-/*var adjustment;
-
-$("ol.simple_with_animation").sortable({
-    group: 'simple_with_animation',
-    pullPlaceholder: false,
-    // animation on drop
-    onDrop: function  ($item, container, _super) {
-        var $clonedItem = $('<li/>').css({height: 0});
-        $item.before($clonedItem);
-        $clonedItem.animate({'height': $item.height()});
-
-        $item.animate($clonedItem.position(), function  () {
-            $clonedItem.detach();
-            _super($item, container);
-        });
-    },
-
-    // set $item relative to cursor position
-    onDragStart: function ($item, container, _super) {
-        var offset = $item.offset(),
-            pointer = container.rootGroup.pointer;
-
-        adjustment = {
-            left: pointer.left - offset.left,
-            top: pointer.top - offset.top
-        };
-
-        _super($item, container);
-    },
-    onDrag: function ($item, position) {
-        $item.css({
-            left: position.left - adjustment.left,
-            top: position.top - adjustment.top
-        });
-    }
-});*/
