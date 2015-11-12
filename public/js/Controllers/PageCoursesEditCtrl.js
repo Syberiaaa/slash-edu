089685be9582c921b2e32d3fb04b0d974e45d46e
mizyukan <mizyukan@mail.ru>
2015-11-12 17:09:02 +0800

    #20 Создать страницу создания/редактирования курса

--- public/js/Controllers/PageCoursesEditCtrl.js
+++ public/js/Controllers/PageCoursesEditCtrl.js
@@ -1,5 +1,5 @@
-PagesControllers.controller('CoursesEditCtrl', ['$scope', '$http',
-    function($scope, $http) {
+PagesControllers.controller('CoursesEditCtrl', ['$scope', '$http', '$location',
+    function($scope, $http, $location) {
         $scope.courseMaterials = [];
 
         $http.get('/api/materials')
@@ -11,14 +11,27 @@ PagesControllers.controller('CoursesEditCtrl', ['$scope', '$http',
         $scope.addCourseMaterial = function(material){
             $scope.courseMaterials.push(material);
             var i = $scope.materials.indexOf(material);
-            $scope.materials.splice(i,1)
-    }
+            $scope.materials.splice(i,1);
+        };
 
         $scope.removeCourseMaterial = function(material) {
             var i = $scope.courseMaterials.indexOf(material);
-            $scope.courseMaterials.splice(i,1)
-            $scope.materials.push(material)
-        }
+            $scope.courseMaterials.splice(i,1);
+            $scope.materials.push(material);
+        };
+
+        $scope.addCourse = function () {
+            $http.put('/api/courses', {
+                name: $scope.name,
+                author: null,
+                materials: $scope.courseMaterials.map(function(it) {return it._id}),
+                instructors: []
+            }).then(function (response) {
+                $location.path('/courses');
+            }).catch(function(err) {
+                console.log(err);
+            });
+        };
 
     }
 ]);
