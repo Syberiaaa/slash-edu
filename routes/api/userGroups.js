1a9255b8254e2f970e890fc74bb77a1f31a02749
Nikolay <lonhcakov2020@mail.ru>
2015-11-12 15:59:34 +0900

    #4

--- routes/api/userGroups.js
+++ routes/api/userGroups.js
@@ -4,13 +4,28 @@ var db = require('../../db');
 var UserGroups = db.model('UserGroups');
 
 router.put('/', function(req, res) {
-    // TODO issue #4
-    res.send('not implemented yet');
+    var newUserGroups = new UserGroups({
+        name: req.body.name,
+        parent:  {
+            src: req.body.src,
+            html: kramed(req.body.src)
+        }
+
+    });
+
+    newUserGroups.save(function(err) {
+
+        if (err) res.sendStatus(400);
+        else res.sendStatus(200);
+    });
 });
 
+
+
 router.get('/', function(req, res) {
-    // TODO issue #4
-    res.send('not implemented yet');
+    UserGroups.find({}, function(err, groups) {
+    res.send(groups);
+    });
 });
 
 router.get('/:groupId', function(req, res) {
@@ -18,13 +33,15 @@ router.get('/:groupId', function(req, res) {
     res.send('not implemented yet');
 });
 
-router.post('/:groupId', function(req, res) {
-    // TODO issue #4
-    res.send('not implemented yet');
+router.post ('/:groupId', function(req, res){
+    UserGroups.success(function (UserGroups) {
+            console.log('UserGroups:', UserGroups);
+        })
+    res.send(UserGroups);
 });
 
 router.delete('/:groupId', function(req, res) {
-    // TODO issue #4
+   req.UserGroups.remove(UserGroups);// TODO issue #4
     res.send('not implemented yet');
 });
 
