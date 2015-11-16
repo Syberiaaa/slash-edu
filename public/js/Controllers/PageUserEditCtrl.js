f584be889ee2d30d423a0430b853bdf55f20f84b
Sergey Moiseenko <sergeymoiseenko.i@mail.ru>
2015-11-16 17:18:09 +0800

    Add PageUserEditCtrl

--- public/js/Controllers/PageUserEditCtrl.js
+++ public/js/Controllers/PageUserEditCtrl.js
@@ -1,5 +1,29 @@
-PagesControllers.controller('UserEditCtrl', ['$scope', '$routeParams',
-    function($scope, $routeParams) {
-        $scope.email = $routeParams.email;
+PagesControllers.controller('UserEditCtrl', ['$scope', '$http', '$routeParams', '$location',
+    function($scope, $http, $routeParams, $location) {
+        $scope.userId = $routeParams.userId;
+        $scope.newPassword = "";
+        $scope.name = "";
+        $scope.email = "";
+        $scope.role = "";
+        $http.get("api/users/:userId").then( function(response) {
+            $scope.name = response.name;
+            $scope.email = response.email;
+            $scope.role = response.role;
+        });
+        $scope.save = function () {
+            var edUser = {};
+            edUser.email = $scope.email;
+            edUser.name = $scope.name;
+            edUser.password = $scope.newPassword;
+            edUser.role = $scope.role;
+            $http.post("api/users/:userId", edUser).then( function(response) {
+            });
+
+        }
+        $scope.delete = function() {
+            $http.delete("api/users/:userId").then(function (response) {});
+            $location.path("/users");
+        }
+
     }
 ]);
